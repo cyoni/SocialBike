bf61034f80fcd7fc3afb43e573bbaff7b2c867ca
Yoni <cyoni10@gmail.com>
2021-08-28 18:41:31 +0300

    udate

--- app/src/main/java/com/example/socialbike/Comment.java
+++ app/src/main/java/com/example/socialbike/Comment.java
@@ -1,6 +1,7 @@
 package com.example.socialbike;
 
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.functions.HttpsCallableResult;
 
 import java.util.ArrayList;
@@ -9,24 +10,31 @@ import java.util.Map;
 
 public class Comment extends Post {
     private final String commentHeadId;
-    private final ArrayList<String> subComments = new ArrayList<>();
+    private final ArrayList<SubComment> subComments = new ArrayList<>();
 
-    public Comment(String container, String commentHeadId, String commentId, String publicKey, String name, int time, String msg) {
-        super(commentId, publicKey, name, time, msg,0, 0, false);
+    public Comment(String container,
+                   String commentHeadId,
+                   String commentId,
+                   String publicKey,
+                   String name,
+                   long time,
+                   String msg) {
+        super(commentId, publicKey, name, time, msg, 0, 0, false);
+        // super(container, postId, commentId )
         this.commentHeadId = commentHeadId;
         this.DatabaseContainer = container;
     }
 
-    public void addSubComment(String subComment){
+    public void addSubComment(SubComment subComment) {
         subComments.add(0, subComment);
     }
 
-    public ArrayList<String> getSubComments(){
+    public ArrayList<SubComment> getSubComments() {
         return subComments;
     }
 
 
-    public Task<HttpsCallableResult> sendSubComment(String message){
+    public Task<HttpsCallableResult> sendSubComment(String message) {
         System.out.println("sending comment. " + message + ". CommentId: " + getPostId() + ", " + DatabaseContainer + ", " + commentHeadId);
         Map<String, Object> data = new HashMap<>();
         data.put("comment", message);
@@ -38,4 +46,18 @@ public class Comment extends Post {
                 .call(data);
     }
 
+    protected void registerLike(boolean state) {
+        setIsLiked(state);
+        DatabaseReference ref = MainActivity.mDatabase.
+                child("global_posts").
+                child(commentHeadId).
+                child("comments").
+                child(getPostId()).
+                child("likes").
+                child(ConnectedUser.getPublicKey());
+        if (state)
+            ref.setValue("ok");
+        else
+            ref.removeValue();
+    }
 }
